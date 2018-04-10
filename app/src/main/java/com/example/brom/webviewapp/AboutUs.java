29346c07afe2c75877dfed0cd324ae68525e4d8b
a17emifu <kamonabe0@gmail.com>
2018-04-10 12:00:08 +0200

    uppdated

--- app/src/main/java/com/example/brom/webviewapp/AboutUs.java
+++ app/src/main/java/com/example/brom/webviewapp/AboutUs.java
@@ -1,41 +1,26 @@
 package com.example.brom.webviewapp;
 
 import android.content.Intent;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    // Create a private member variable that can hold our WebView
+public class AboutUs extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        /* This code is run when the App is created. Include code that creates your WebView */
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_about_us);
 
-        WebView myWebView = (WebView) findViewById(R.id.mywebview);
-        myWebView.setWebViewClient(new WebViewClient());
-        myWebView.loadUrl("https://www.google.com/");
-        // 1. Create a WebView element in the layout file content_main.xml
-        // 2. Give the WebView element created in step 1 ID "my_webview"
-        // Enter your code that creates your WebView here...
-        // 3. Locate the WebView element created in step 1 using the ID created in step 2
-        // 4. Create a new WebViewClient to attach to our WebView. This allows us to
-        //    browse the web inside our app.
-        // 5. Enter the url to load in our WebView
+        WebView mysecondView = (WebView) findViewById(R.id.secondview);
+        mysecondView.setWebViewClient(new WebViewClient());
+        mysecondView.loadUrl("file:///android_asset/about.html");
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -63,4 +48,11 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void onClick( View v_ )
+    {
+        finish();
+    }
 }
+
+
